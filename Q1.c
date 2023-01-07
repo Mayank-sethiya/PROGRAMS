7855f84ac4476d7ba4d4cddaa710f53d8e13907c
Mayank-sethiya <114764579+Mayank-sethiya@users.noreply.github.com>
2023-01-07 18:20:28 +0530

    Add files via upload
    C PROGRAM TO PERFORM ALL ARITHMETIC OPERATIONS

---
+++ Q1.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("ENETER THE VALUES OF A AND B\n");
+	scanf("%d%d",&a,&b);
+	int c=a+b;
+	int d=a-b;
+	int e=a*b;
+	int f=a/b;
+	int g=a%b;
+	printf("ADDITION=%d \n subtraction=%d \n MULTIPLICATION=%d \n DIVISION=%d \n MODULUS=%d",c,d,e,f,g);
+}
